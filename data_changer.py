1edaf9992f77cfe3c6c4ddbd4d96c4c7536a1015
gorbachp <gorbachonline@gmailc.om>
2025-01-13 01:41:24 +0500

    data_changer

--- data_changer.py
+++ data_changer.py
@@ -1,4 +1,4 @@
 created_date = "19-03-2024"
 issue_date = "19-04-2024"
 print("Дата создания: ", created_date[0:5])
-print("Дата окончания: ", issue_date[0:5])
+print("Дата окончани: ", issue_date[0:5])
