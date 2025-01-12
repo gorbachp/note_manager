c92bc7e0dfcd155ff88193a817fd20dc6ff95d02
gorbachp <gorbachonline@gmailc.om>
2025-01-12 23:02:23 +0500

    greetings

---
+++ greetings.py
@@ -0,0 +1,18 @@
+username = 'Алекс'
+print('Имя пользователя:', username)
+
+title = 'Отдых'
+print('Заголовок заметки:', title)
+
+content = 'Устроить пикник с друзьями'
+print('Описание заметки:', content)
+
+status = 'Не выполнена'
+print('Статус заметки:', status)
+
+created_date = '15-06-2025'
+print('Дата создания:', created_date)
+
+issue_date = '22-06-2025'
+print('Дата окончания:', issue_date)
+
