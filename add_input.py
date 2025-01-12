379155c33c033f68650eaefa35a17907aa594fe6
gorbachp <gorbachonline@gmailc.om>
2025-01-13 01:39:12 +0500

    add input

---
+++ add_input.py
@@ -0,0 +1,11 @@
+username = input('Имя: ')
+
+title = input('Заголовок заметки: ')
+
+content = input('Описание заметки: ')
+
+status = input('Статус заметки: ')
+
+created_date = int(input('Дата создания заметки: '))
+
+issue_date = int(input('Дата окончания заметки: '))
