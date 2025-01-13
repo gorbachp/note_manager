8a073b0ba159675b3aa26a91973d3d56e83a0cfb
gorbachp <gorbachonline@gmailc.om>
2025-01-13 19:05:12 +0500

    add list

---
+++ add_list.py
@@ -0,0 +1,7 @@
+title1 = input('Введите 1-ый заголовок заметки: ')
+title2 = input('Введите 2-ой заголовок заметки: ')
+title3 = input('Введите 3-ий заголовок заметки: ')
+title_list = [title1, title2, title3]
+
+print("Заголовки заметки: ", title_list)
+
