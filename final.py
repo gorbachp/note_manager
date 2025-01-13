5f1bbad166aebdc41593d42cf78743f71e718153
gorbachp <gorbachonline@gmailc.om>
2025-01-13 20:01:56 +0500

    final

---
+++ final.py
@@ -0,0 +1,28 @@
+#Вводные данные пользователя
+username = input('Введите ваше имя: ')
+title1 = input('Введите заголовок 1-ой заметки: ')
+title2 = input('Введите заголовок 2-ой заметки: ')
+title3 = input('Введите заголовок 3-ей заметки: ')
+title_list = (title1, title2, title3)
+content = input('Опишите заметку: ')
+status = input('Введите статус заметки: ')
+created_date = input('Напишите дату создания заметки (в формате дд-мм-гггг): ')
+issue_date = input('Напишите дату окончания заметки (в формате дд-мм-гггг): ')
+
+#Список ввдеенных данных от пользвателя
+note = [
+username,
+title_list, # список для заголовков
+content,
+status,
+created_date,
+issue_date
+]
+
+#Вывод вводных данных пользователя
+print('Ваше имя:', username)
+print('Заголовки заметки:', title_list)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания:', created_date)
+print('Дата окончания:', issue_date)
